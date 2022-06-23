d6dce5441e8be281a1afbbf232046ed10e3a4934
Mario1613 <mariojhaziel25@gmail.com>
2022-06-23 14:13:57 -0500

    firstcommit

---
+++ src/todo/components/TodoList.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { TodoContext } from "../context/TodoContext";
+import { TodoState } from "../interfaces/interfaces";
+
+export const TodoList = () =>{
+
+    const {todoState} = useContext(TodoContext);
+
+    return (
+        <ul>
+wegf
+        </ul>
+    )
+}
+
+
+
+
+
